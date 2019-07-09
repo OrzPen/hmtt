490e3a9a0fd3584f26e8c0775845bc8916bb279b
OrzPen <529234641@qq.com>
2019-07-09 17:58:24 +0800

    login背景与卡片

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="container">Login</div>
+    <div class="login-container">
+        <el-card class="login_box">
+            <img src="../../assets/images/logo_index.png" alt="">
+        </el-card>
+    </div>
 </template>
 
 <script>
@@ -8,6 +12,27 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang='less'>
+.login-container {
+    width:100%;
+    height:100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+    .login_box {
+        width: 450px;
+        height: 350px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50% , -50%);
+        img{
+            display: block;
+            width: 200px;
+            margin: 0 auto;
+        }
+    }
+}
 
 </style>
