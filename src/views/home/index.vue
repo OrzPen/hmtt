9db02422ab4b655dc5f095bcc38046208a26e653
OrzPen <529234641@qq.com>
2019-07-11 19:29:35 +0800

    首页模块-基础布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>主页</div>
+  <el-container class="home-container">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style scoped lang='less'>
+.home-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
 </style>
