753ea228ac8c9d77d7124fbfa0cc985df6f44e1c
OrzPen <529234641@qq.com>
2019-07-11 21:25:01 +0800

    首页模块-欢迎页面

---
+++ src/views/wecome/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        <img src="../../assets/images/welcome.jpg" alt="" class="welcomepic">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang='less'>
+.main {
+    .welcomepic{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+}
+
+</style>
