907928526bc6dec587f0e42bb7181c46fc87e0ed
OrzPen <529234641@qq.com>
2019-07-11 19:25:29 +0800

    首页模块-路由与组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/login/index.vue'
+// 导入首页模块
+import Home from '../views/home'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    // 配置首页路由规则
+    { name: 'home', path: '/', component: Home }
   ]
 })
 export default router
