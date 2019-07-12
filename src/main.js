aa8b02ac4a5287f93fd6918939f5ec26b6d0150d
OrzPen <529234641@qq.com>
2019-07-12 19:38:02 +0800

    axios-默认配置选项

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router/index'
 import axios from 'axios'
+// 设置axios基准路径,默认加载路径前的路径
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// 设置请求头
+axios.defaults.headers = {
+  // token认证需要的字段,值前面注意加上前缀bearer+空格
+  Authorization: 'Bearer ' + JSON.parse(window.sessionStorage.getItem('hmtt')).token
+}
 Vue.use(ElementUI)
 Vue.prototype.$ajax = axios
 Vue.config.productionTip = false
