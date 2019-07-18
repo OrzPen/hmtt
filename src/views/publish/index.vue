029b7e62e8f020bb41c7ee3faf121fb56e55bdcf
OrzPen <529234641@qq.com>
2019-07-18 15:36:49 +0800

    发布文章-富文本配置完毕

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -8,7 +8,10 @@
         <el-form-item label="标题：">
           <el-input v-model="articleForm.title" style="width:300px"></el-input>
         </el-form-item>
-        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="内容：">
+          <!-- 富文本编辑器 -->
+           <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
+        </el-form-item>
         <el-form-item label="封面：">
           <el-radio-group v-model="articleForm.cover.type">
             <el-radio :label="1">单图</el-radio>
@@ -33,16 +36,39 @@
 </template>
 
 <script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
 export default {
+  components: { quillEditor },
   data () {
     return {
       articleForm: {
+        // 富文本编辑器中的内容
+        content: null,
+        // 标题内容对应的数据
         title: '',
         cover: {
+          // 单选按钮默认选项
           type: 1,
           images: []
         },
+        // 频道信息ID
         channel_id: null
+      },
+      // 富文本编辑器配置选项
+      editorOption: {
+        placeholder: '',
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ 'header': 1 }, { 'header': 2 }],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }]
+          ]
+        }
       }
     }
   }
@@ -50,5 +76,4 @@ export default {
 </script>
 
 <style>
-
 </style>
