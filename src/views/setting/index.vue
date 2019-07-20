862a59cd30d3ff9388a0c0a10ccdec0dc75bdc8f
OrzPen <529234641@qq.com>
2019-07-20 20:49:45 +0800

    个人设置-用户信息渲染

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -8,13 +8,16 @@
         <el-col :span="12">
           <!-- 表单 -->
           <el-form :model="userForm">
-            <el-form-item label="编号：">100</el-form-item>
-            <el-form-item label="手机：">13200000000</el-form-item>
+            <el-form-item label="编号：">{{userForm.id}}</el-form-item>
+            <el-form-item label="手机：">{{userForm.mobile}}</el-form-item>
+            <el-form-item label="邮箱：">
+              <el-input v-model="userForm.email"></el-input>
+            </el-form-item>
             <el-form-item label="媒体名称：">
-              <el-input></el-input>
+              <el-input v-model="userForm.name"></el-input>
             </el-form-item>
             <el-form-item label="媒体介绍：">
-              <el-input type="textarea" :rows="3"></el-input>
+              <el-input type="textarea" v-model="userForm.intro" :rows="3"></el-input>
             </el-form-item>
             <el-form-item>
               <el-button type="primary">保存设置</el-button>
@@ -28,7 +31,7 @@
             action="https://jsonplaceholder.typicode.com/posts/"
             :show-file-list="false"
           >
-            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <img v-if="userForm.photo" :src="userForm.photo" class="avatar" />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
           </el-upload>
         </el-col>
@@ -42,8 +45,25 @@
 export default {
   data () {
     return {
-      userForm: {},
+      userForm: {
+        id: null,
+        name: null,
+        intro: null,
+        photo: null,
+        email: null,
+        mobile: null
+      },
       imageUrl: null
+    }
+  },
+  created () {
+    // 获取用户资料
+    this.getUser()
+  },
+  methods: {
+    async getUser () {
+      const { data: { data } } = await this.$ajax.get('user/profile')
+      this.userForm = data
     }
   }
 }
